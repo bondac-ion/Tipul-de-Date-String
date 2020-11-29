ff3f087bf0f550f3b5fba29d7cd59d81feeb3365
bondac-ion <70916383+bondac-ion@users.noreply.github.com>
2020-11-29 17:14:20 +0300

    Add files via upload

---
+++ str6.py
@@ -0,0 +1,6 @@
+
+a=str(input("dati un cuvint:"))
+b=str(input("dati o litera:"))
+for i in a:
+    c=a.replace(i,b)
+    print(c)
---
+++ str7.py
@@ -0,0 +1,7 @@
+import string
+a=str(input("dati un nume de persoana:"))
+b=a.title()
+if a==b:
+    print("Poate fi un nume")
+else:
+    print("Nu poate fi un nume")
